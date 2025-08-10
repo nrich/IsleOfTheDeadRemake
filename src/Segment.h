8dd39341f94c98e80f54b3467e4c7d5c768a266e
Neil Richardson <nrich@neiltopia.com>
2025-08-10 17:59:02 +0800

    Finally clean up segment code

--- src/Segment.h
+++ src/Segment.h
@@ -37,12 +37,7 @@ struct Segment {
     uint16_t flags;
     uint16_t count;
 
-    Segment(size_t _id, uint16_t _x1, uint16_t _y1, uint16_t _x2, uint16_t _y2, uint16_t _texture, uint16_t _flags, uint16_t _count) : id(_id), x1(_x1), y1(_y1), x2(_x2), y2(_y2), texture(_texture), flags(_flags), count(_count) {
-    }
-
-    void render(const raylib::Camera3D &camera, uint64_t frame_count) const;
-
-    std::pair<raylib::Vector2, float> getBounds() const;
+    Segment(size_t id, uint16_t x1, uint16_t y1, uint16_t x2, uint16_t y2, uint16_t texture, uint16_t flags, uint16_t count);
 };
 
 #endif //SEGMENT_H
